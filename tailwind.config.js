f17439fd1513f8e992902fb6e0a0cea8b157824b
Enmanuel Cruz <rember98cjs@gmail.com>
2024-03-24 17:21:25 -0600

    routes

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        red_1: "red",
+      },
+    },
+  },
+  plugins: [],
+};
